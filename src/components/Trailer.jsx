c9ccc4cce36cccaefb54aea98d0447878b24e2e0
depeche00777 <99623896+depeche00777@users.noreply.github.com>
2022-12-19 11:57:56 +0900

    Update Trailer.jsx

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,5 +1,6 @@
 //react-bootstrap modal을 이용한 유튜브 팝업
 //https://www.npmjs.com/package/react-youtube -react-youtube라이브러리 설치
+//https://github.com/u-wave/react-youtube  -react-youtube라이브러리
 
 import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
@@ -8,9 +9,22 @@ import YouTube from "react-youtube";
 const Trailer = ({ item }) => {
 	const [show, setShow] = useState(false);
 
+	//"Official Trailer"가 있을때
+	const trailer = item.results?.find((item) => {
+		if (item.name === "Official Trailer") {
+			return item;
+		}
+	});
+
+	// type에 "Trailer"가 있을때
+	const trailer2 = item.results?.find((item) => {
+		if (item.type === "Trailer") {
+			return item;
+		}
+	});
+
 	const opts = {
 		playerVars: {
-			// https://developers.google.com/youtube/player_parameters
 			autoplay: 0,
 		},
 	};
@@ -20,15 +34,8 @@ const Trailer = ({ item }) => {
 		event.target.pauseVideo();
 	};
 
-	//{item.results[0].key} -  무조건 첫번째 유투브를 보여줌
-	//"Official Trailer"가 있을때
-	const official = item.results?.find((item) => {
-		return item.name === "Official Trailer";
-	});
-
 	return (
 		<div>
-			{console.log("받아온 item이 모야?? ", item)}
 			<p className="trailer-btn" onClick={() => setShow(true)}>
 				🎬 Watch Trailer
 			</p>
@@ -43,7 +50,7 @@ const Trailer = ({ item }) => {
 				<Modal.Header closeButton></Modal.Header>
 				<Modal.Body>
 					<YouTube
-						videoId={official?.key || item.results[0].key}
+						videoId={trailer?.key ? trailer?.key : trailer2?.key}
 						opts={opts}
 						onReady={_onReady}
 					/>
